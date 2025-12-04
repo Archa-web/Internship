a2f29368618dd469e4d93f87caeb1ef6eafb99c6
Archa <archams66@gmail.com>
2025-12-04 21:15:29 +0530

    Add my file

---
+++ comments.py
@@ -0,0 +1,13 @@
+# Single line comment
+# print("I love python")
+
+# multi-line comment (Double quotes)
+"""
+print("This is my first day")
+print("I am from kerala")
+"""
+
+# multi-line comment (single quotes)
+'''
+print("my name is archa")
+'''
---
+++ variable.py
@@ -0,0 +1,17 @@
+# Storing string in variable
+name = "Archa"
+print(name)
+
+# Storing number in variable
+num = 25
+print(num)
+
+# Case sensitivity
+name = "John"
+Name = "Jane"
+NAME = "Jack"
+
+# All three are DIFFERENT variable
+print(name)
+print(Name)
+print(NAME)
