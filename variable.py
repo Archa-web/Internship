1e2e5cc1c3442dfc136c53f9cd3847307c7ce6ca
Archa <archams66@gmail.com>
2025-12-04 21:25:17 +0530

    Add my file

--- variable.py
+++ variable.py
@@ -12,6 +12,28 @@ Name = "Jane"
 NAME = "Jack"
 
 # All three are DIFFERENT variable
+print(name) # John
+print(Name) # Jane
+print(NAME) # Jack
+
+# Assigning same value to diff variables
+a = 2
+b = 2
+c = 2
+
+a = b = c = 2
+print(a)
+print(b)
+print(c)
+
+# Assign multiple values to multiple variables
+a, b, c = 1, 2, 3
+print(a)
+print(b)
+print(c)
+
+# Assign different data type values to multiple variables
+name, age, email = "Archa", 25, "archa@gmail.com"
 print(name)
-print(Name)
-print(NAME)
+print(age)
+print(email)
